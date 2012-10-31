579b5ca5a269188391a00f094c832ec71c95a1ed
Kjersten Elias <kjersten.elias@gmail.com>
2012-10-31 16:50:55 +0700

    adjusting build.gradle so that it doesn't include the config files in the jar...  the project isn't working in Eclipse because of complie/classpath issues for the config files

--- src/main/scala/net/bitzoo/dayone/AutoEntry.scala
+++ src/main/scala/net/bitzoo/dayone/AutoEntry.scala
@@ -5,22 +5,26 @@ package net.bitzoo.dayone {
 
   object AutoEntry extends App {
 
-    val today = DateTime.now().toString("yyyy-MM-dd")
-    val dateFileName = "MostRecentEntryMadeOn.txt"
-    val dateFile = new java.io.File(ClassLoader.getSystemResource(dateFileName).toURI())
-    val templateFileName = "template.txt"
-
     // use to debug the classpath
     val urls = ClassLoader.getSystemClassLoader().asInstanceOf[java.net.URLClassLoader].getURLs()
     for (url <- urls) { Console.println(url) }
     
+    val templateFileName = "template.txt"
+    val today = DateTime.now().toString("yyyy-MM-dd")
+    val dateFileName = "MostRecentEntryMadeOn.txt"
+    val dateFile = new java.io.File(ClassLoader.getSystemResource(dateFileName).toURI())
+    
     val dateFileStream = scala.io.Source.fromFile(dateFile)
     val mostRecentEntryDate = dateFileStream.mkString
     dateFileStream.close()
+    Console.println("wtf")
 
     if (!today.equals(mostRecentEntryDate)) {
+      Console.println("hello")
+      
       // create a new entry in DayOne with the contents of the template file
-      "/usr/local/bin/dayone new" #< new java.io.File(ClassLoader.getSystemResource(templateFileName).toURI()) !
+      val swallowOutput = "/usr/local/bin/dayone new" #< 
+      	new java.io.File(ClassLoader.getSystemResource(templateFileName).toURI()) !
 
       // record the fact that my template file has been created
       AutoEntryUtil.writeToFile(dateFile, today)
