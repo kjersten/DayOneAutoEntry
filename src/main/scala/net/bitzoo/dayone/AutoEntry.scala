56411e8d73da492d8b899502a35e57e6ea634d75
Kjersten Elias <kjersten.elias@gmail.com>
2012-11-01 14:49:34 +0700

    updated project so that I don't get a mail message every time a new DayOne entry is created - that message will now be routed to a dead-end output stream

--- src/main/scala/net/bitzoo/dayone/AutoEntry.scala
+++ src/main/scala/net/bitzoo/dayone/AutoEntry.scala
@@ -5,26 +5,26 @@ package net.bitzoo.dayone {
 
   object AutoEntry extends App {
 
-    // use to debug the classpath
-//    val urls = ClassLoader.getSystemClassLoader().asInstanceOf[java.net.URLClassLoader].getURLs()
-//    for (url <- urls) { Console.println(url) }
-
     val today = DateTime.now().toString("yyyy-MM-dd")
     val templateFileName = "config/template.txt"
     val dateFileName = "config/MostRecentEntryMadeOn.txt"
+      
+    // find out when the most recent DayOne template entry was made
     val dateFile = new java.io.File(ClassLoader.getSystemResource(dateFileName).toURI())
-
     val dateFileStream = scala.io.Source.fromFile(dateFile)
     val mostRecentEntryDate = dateFileStream.mkString
     dateFileStream.close()
 
+    // if a DayOne template entry wasn't made today...
     if (!today.equals(mostRecentEntryDate)) {
-
+      
       // create a new entry in DayOne with the contents of the template file
-      val swallowOutput = "/usr/local/bin/dayone new" #<
-        new java.io.File(ClassLoader.getSystemResource(templateFileName).toURI()) !
+      // write the output to a dead-end byte stream so nothing is written to console/sent to mail
+      "/usr/local/bin/dayone new" #<
+        new java.io.File(ClassLoader.getSystemResource(templateFileName).toURI()) #>
+      	new java.io.ByteArrayOutputStream() !
 
-      // record the fact that my template file has been created
+      // record the fact that the template entry has been created
       AutoEntryUtil.writeToFile(dateFile, today)
     }
 
